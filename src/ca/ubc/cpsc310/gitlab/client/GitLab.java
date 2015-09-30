97ddbc0d6198e7210b66d49fccf32cc076a7a65c
Graham McAllister <gmcallister@alumni.ubc.ca>
2015-09-30 12:12:48 -0700

    Re-added comment

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -69,6 +69,7 @@ public class GitLab implements EntryPoint {
 
 		RootPanel.get("root").add(flexTable);
 		
+		// Added comment: "A"
 		flexTable.setText(0,0, "Name");
 		flexTable.setText(0,1,"Language");
 		flexTable.setText(0,2, "Size of Shopping Cart");
